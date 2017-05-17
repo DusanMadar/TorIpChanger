ad034f93d11e96665e167413a8242beeedcaa54b
Dusan Madar <madar.dusan@gmail.com>
2017-05-17 16:46:07 +0200

    Config - remove stale config variable

--- toripchanger/config.py
+++ toripchanger/config.py
@@ -1,8 +1,9 @@
-# TOR settings
+# TOR settings.
 TOR_PASSWORD = ''
 TOR_PORT = 9051
+
+# Proxy settings.
 LOCAL_HTTP_PROXY = {'http': '127.0.0.1:8118'}
 
-# IP obtaining settings
-CURRENT_IP_TIMEOUT = 5
+# IP obtaining settings.
 NEW_IP_MAX_ATTEMPTS = 10
